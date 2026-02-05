45052670448ea271af9baba1203fb810a31257e1
matvey <werkermatvey@gmail.com>
2026-02-05 23:02:56 +0700

    auth | bloc | Подключение AuthBloc к UI

--- lib/src/feature/auth/presentation/screens/reset_password_screen.dart
+++ lib/src/feature/auth/presentation/screens/reset_password_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sfu/src/core/localization/app_localizations.dart';
+import 'package:sfu/src/feature/auth/presentation/bloc/auth_bloc.dart';
 
 class ResetPasswordScreen extends StatelessWidget {
   const ResetPasswordScreen({super.key});
@@ -16,43 +18,69 @@ class ResetPasswordScreen extends StatelessWidget {
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 45, vertical: 100),
-            child: Column(
-              children: [
-                SvgPicture.asset(
-                  'assets/images/logo.svg',
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.primary,
-                    BlendMode.srcIn,
-                  ),
+      child: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            authorized: () => Navigator.pushReplacementNamed(context, '/home'),
+            unauthorized: () =>
+                Navigator.pushReplacementNamed(context, '/signIn'),
+            error: (message) => {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(message))),
+            },
+            orElse: () => {},
+          );
+        },
+        builder: (context, state) {
+          return Scaffold(
+            resizeToAvoidBottomInset: true,
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45, vertical: 100),
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.primary,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: t?.passwordHint),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: password2Controller,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: t?.passwordRepeatHint,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                          AuthEvent.resetPassword(
+                            newPassword: passwordController.text,
+                          ),
+                        );
+                      },
+                      child: state.maybeWhen(
+                        loading: () => CircularProgressIndicator(),
+                        orElse: () => Text(t!.resetPasswordButton),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 50),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: t?.passwordHint),
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: password2Controller,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: t?.passwordRepeatHint),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/signIn');
-                  },
-                  child: Text(t!.resetPasswordButton),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
--- lib/src/feature/auth/presentation/screens/sign_up_screen.dart
+++ lib/src/feature/auth/presentation/screens/sign_up_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:sfu/src/core/localization/app_localizations.dart';
+import 'package:sfu/src/feature/auth/presentation/bloc/auth_bloc.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -19,56 +21,88 @@ class SignUpScreen extends StatelessWidget {
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 45, vertical: 100),
-            child: Column(
-              children: [
-                SvgPicture.asset(
-                  'assets/images/logo.svg',
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.primary,
-                    BlendMode.srcIn,
-                  ),
+      child: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            authorized: () => Navigator.pushReplacementNamed(context, '/home'),
+            unauthorized: () =>
+                Navigator.pushReplacementNamed(context, '/signIn'),
+            error: (message) => {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(message))),
+            },
+            orElse: () => {},
+          );
+        },
+        builder: (context, state) {
+          return Scaffold(
+            resizeToAvoidBottomInset: true,
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 45, vertical: 100),
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.primary,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(hintText: t?.nameHint),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: surnameController,
+                      decoration: InputDecoration(hintText: t?.surnameHint),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: loginController,
+                      decoration: InputDecoration(hintText: t?.loginHint),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(hintText: t?.passwordHint),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      controller: password2Controller,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: t?.passwordRepeatHint,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                          AuthEvent.signUp(
+                            login: loginController.text,
+                            password1: passwordController.text,
+                            password2: password2Controller.text,
+                            firstName: nameController.text,
+                            lastName: surnameController.text,
+                          ),
+                        );
+                      },
+                      child: state.maybeWhen(
+                        loading: () => CircularProgressIndicator(),
+                        orElse: () => Text(t!.signUpButton),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 50),
-                TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(hintText: t?.nameHint)),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: surnameController,
-                  decoration: InputDecoration(hintText: t?.surnameHint),
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                    controller: loginController,
-                    decoration: InputDecoration(hintText: t?.loginHint)),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: t?.passwordHint),
-                ),
-                const SizedBox(height: 24),
-                TextField(
-                  controller: password2Controller,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: t?.passwordRepeatHint),
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/signIn');
-                  },
-                  child: Text(t!.signUpButton),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
