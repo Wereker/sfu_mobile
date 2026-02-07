f9236eb19d4a5885a6f88ae7a619afd79f595ffe
matvey <werkermatvey@gmail.com>
2026-02-07 17:23:36 +0700

    home | UI | Перенос Главного экрана в общую папку

---
+++ lib/src/app/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sfu/src/app/widgets/app_navigation_bar.dart';
+import 'package:sfu/src/app/widgets/custom_app_bar.dart';
+import 'package:sfu/src/core/localization/app_localizations.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  final List<Widget> screens = [
+    Scaffold(body: const Center(child: Text("Список новостей"))),
+    Scaffold(body: const Center(child: Text("Расписание занятий"))),
+    Scaffold(body: const Center(child: Text("Чат"))),
+    Scaffold(body: const Center(child: Text("Календарь"))),
+    Scaffold(body: const Center(child: Text("Домашняя"))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final t = AppLocalizations.of(context);
+
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: IndexedStack(index: currentIndex, children: screens),
+      bottomNavigationBar: AppNavigationBar(
+        currentIndex: currentIndex,
+        onDestinationSelected: (index) {
+          setState(() => currentIndex = index);
+        },
+      ),
+    );
+  }
+}
