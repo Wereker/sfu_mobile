3997c622892c04847d85b540bc03440ee6fc8a23
matvey <werkermatvey@gmail.com>
2026-02-12 21:08:17 +0700

    core | Добавление splash-экрана

---
+++ lib/src/core/widgets/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/images/logo.svg',
+          colorFilter: ColorFilter.mode(
+            Theme.of(context).colorScheme.primary,
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+    );
+  }
+}
